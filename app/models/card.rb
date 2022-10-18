73033755d0046d04d11dfb074a5ebc651194c909
Timaio <timeiqu@gmail.com>
2022-10-18 06:32:10 +0300

    Buy transaction fix

--- app/models/card.rb
+++ app/models/card.rb
@@ -13,34 +13,38 @@ class Card < ApplicationRecord
     amount_due = amount
     remaining_bonus = self.bonuses
 
-    # Withdraw bonuses
-    if use_bonuses
-      user = self.user
+    user = self.user
 
-      bonus_balance = user.negative_balance ? user.cards.sum(:bonuses) : remaining_bonus
+    Card.transaction do
+      all_cards_bonuses = Card.lock.where(user_id: user.id).map(&:bonuses).sum
 
-      if bonus_balance <= amount
-        if bonus_balance > 0
-          amount_due = amount.round(half: :up) - bonus_balance
+      # Withdraw bonuses
+      if use_bonuses
+        bonus_balance = user.negative_balance ? all_cards_bonuses : remaining_bonus
+
+        if bonus_balance <= amount
+          if bonus_balance > 0
+            amount_due = amount.round(half: :up) - bonus_balance
+          end
+
+          remaining_bonus -= bonus_balance
+        else
+          amount_due = 0
+          remaining_bonus -= amount.round(half: :up)
         end
-
-        remaining_bonus -= bonus_balance
-      else
-        amount_due = 0
-        remaining_bonus -= amount.round(half: :up)
       end
+
+      # Add bonuses
+      if amount >= MIN_AMOUNT_TO_ADD_BONUSES
+        remaining_bonus += (amount_due * BONUS_PERCENTAGE).to_i
+      end
+
+      self.update(bonuses: remaining_bonus)
+
+      return {
+        amount_due: amount_due,
+        remaining_bonus: remaining_bonus
+      }
     end
-
-    # Add bonuses
-    if amount >= MIN_AMOUNT_TO_ADD_BONUSES
-      remaining_bonus += (amount_due * BONUS_PERCENTAGE).to_i
-    end
-
-    self.update(bonuses: remaining_bonus)
-
-    return {
-      amount_due: amount_due,
-      remaining_bonus: remaining_bonus
-    }
   end
 end
